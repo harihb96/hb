3bf4bfec8db8fe328ad5b1d12e53bd94b319bdcd
harihb96 <hari199627@gmail.com>
2017-06-16 23:04:44 -0700

    Create Check_Even.java

---
+++ Check_Even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Check_Even
+{
+public static void main(String[] args)
+{
+int a;
+Scanner s = new Scanner(System.in);
+System.out.println("enter the variables");
+a=s.nextInt();
+if((a%2)==0)
+{
+System.out.println("the number"+a"is even");
+}
+else 
+{
+System.out.println("the number"+a"is odd");
+}
+}
+}
