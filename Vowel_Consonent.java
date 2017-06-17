e2ef72fbe3d591be7f68df760c30763b6316aa8c
harihb96 <hari199627@gmail.com>
2017-06-17 00:20:45 -0700

    Update Vowel_Consonent.java

--- Vowel_Consonent.java
+++ Vowel_Consonent.java
@@ -7,7 +7,7 @@ public static void main(String[] args)
 char ch;
 Scanner s = new Scanner(System.in);
 System.out.println("enter the variables");
- ch = s.next().charAt();
+ ch = s.next().charAt(0);
 if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
 {
 System.out.println(ch+" is vowel");
