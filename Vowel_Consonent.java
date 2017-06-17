6478b3a514f73fc540bf5ae49ed1fa936c2e9326
harihb96 <hari199627@gmail.com>
2017-06-17 00:16:53 -0700

    Create Vowel_Consonent.java

---
+++ Vowel_Consonent.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+public class Vowel_Consonent
+{
+public static void main(String[] args)
+{
+char ch;
+Scanner s = new Scanner(System.in);
+System.out.println("enter the variables");
+ ch = s.next().charAt();
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+{
+System.out.println(ch+" is vowel");
+}
+else if(ch>='a' && ch<='z')
+{
+System.out.println(ch+" is consonent");
+}
+else
+{
+System.out.println(ch+" is not an alphabet");
+}}
+}
